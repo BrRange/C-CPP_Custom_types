3ae0acaf8f2af60dfec274591ea4502185ac8d1b
BrRange <adressphsp@gmail.com>
2025-10-30 00:06:56 -0300

    Finally some updates

---
+++ darray.c
@@ -0,0 +1,78 @@
+#include "darray.h"
+
+void darray_grow(void *darrayGeneric, usz target, usz typeSize){
+  darrayTemplate(void) *darray = darrayGeneric;
+  if(!darray->cap){
+    darray->cap = 2;
+    darray->data = safeRealloc(darray->data, 2 * typeSize);
+    return;
+  }
+  bool changed = false;
+  while(darray->cap < target){
+    darray->cap *= 2;
+    changed = true;
+  }
+  if(changed) darray->data = safeRealloc(darray->data, darray->cap * typeSize);
+}
+
+void darray_shrink(void *darrayGenereric, usz typeSize){
+  darrayTemplate(void) *darray = darrayGenereric;
+  while(darray->cap / 2 > darray->size) darray->cap /= 2;
+  darray->data = safeRealloc(darray->data, typeSize * darray->cap);
+}
+
+void darray_append(void *darrayGeneric, void *element, usz typeSize){
+  darrayTemplate(void) *darray = darrayGeneric;
+  darray_grow(darrayGeneric, darray->size + 1, typeSize);
+  u8 (*sized)[typeSize] = darray->data;
+  memcpy(sized + darray->size, element, typeSize);
+  ++darray->size;
+}
+
+void darray_appendPtr(void *darrayGeneric, void *element){
+  darray_append(darrayGeneric, &element, sizeof element);
+}
+
+void darray_appendMany(void *darrayGeneric, void *elementList, usz elementCount, usz typeSize){
+  darrayTemplate(void) *darray = darrayGeneric;
+  darray->size += elementCount;
+  darray_grow(darray, darray->size, typeSize);\
+  u8 (*sized)[typeSize] = darray->data;
+  memcpy(sized + darray->size - elementCount, elementList, elementCount * typeSize);
+}
+
+void darray_remove(void *darrayGeneric, usz index, usz typeSize){
+  darrayTemplate(void) *darray = darrayGeneric;
+  u8 (*sized)[typeSize] = darray->data;
+  if(index < darray->size - 1)
+    memmove(
+      sized + index,
+      sized + index + 1,
+      (darray->size - index - 1) * typeSize\
+    );
+  --darray->size;
+}
+
+void darray_removeMany(void *darrayGeneric, usz index, usz amount, usz typeSize){
+  darrayTemplate(void) *darray = darrayGeneric;
+  u8 (*sized)[typeSize] = darray->data;
+  if(index < darray->size - amount)\
+    memmove(\
+      sized + index,\
+      sized + index + amount,\
+      (darray->size - index - amount) * typeSize\
+    );\
+  darray->size -= amount;\
+}
+
+void darray_destroy(void *darrayGeneric){
+  darrayTemplate(void) *darray = darrayGeneric;
+  free(darray->data);
+  memset(darray, 0, sizeof(*darray));
+}
+
+void darray_recFree(void *darrayGeneric){
+  darrayTemplate(void*) *darray = darrayGeneric;
+  for(usz i = 0; i < darray->size; ++i)
+    free(darray->data[i]);
+}
--- darray.h
+++ darray.h
@@ -1,3 +1,8 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "../rustydef.h"
+
 #ifndef DARRAYH
 #define DARRAYH
 
@@ -5,56 +10,109 @@
 Expected format:
 {
     type *data;
-    u64 size, cap;
+    u32 size, cap;
+    ...
 }
 
 Expected new data:
-{0, 0, NULL}
+{0} || {0, 0, NULL, ...}
 */
 
-#include <stdlib.h>
-#include <string.h>
-#include "rustydef.h"
-
-void *arrRealloc(void *ptr, int *size, int targ){
-  void *newPtr = realloc(ptr, targ);
-  if(newPtr){
-    *size = targ;
-    return newPtr;
+static void *safeRealloc(void *ptr, int targ){
+  retry: void *newPtr = realloc(ptr, targ);
+  if(!newPtr){
+    puts("Allocation error\nRetrying...");
+    goto retry;
   }
-  return ptr;
+  return newPtr;
 }
 
-#define darrayGrow(da) do{\
-  if(!(da).cap){\
-    (da).cap = 2ull;\
-  } else{\
-    (da).cap += (da).cap < 8 ? (da).cap : 8ull;\
+#define darrayTemplate(_type) struct{\
+  _type *data;\
+  usz size, cap;\
+}
+
+#define darrayGrow(_da, _target) do{\
+  if(!(_da).cap){\
+    (_da).cap = 2ull;\
+    (_da).data = realloc((_da).data, sizeof(*(_da).data) * 2);\
+    break;\
   }\
-  (da).data = realloc((da).data, sizeof(*(da).data) * (da).cap);\
-} while(0)
-
-#define darrayShrink(da) do{\
-  u64 newCap = (da).cap > 8 ? (da).cap - 8ull : (da).cap >> 1ull;\
-  while(newCap >= (da).size){\
-    (da).cap = newCap;\
-    newCap = (da).cap > 8 ? (da).cap - 8ull : (da).cap >> 1ull;\
+  bool _changed = false;\
+  while((da).cap < (_target)){\
+    (da).cap *= 2;\
+    _changed =  true;\
   }\
-  (da).data = realloc((da).data, (da).cap * sizeof(*(da).data));\
+  (_da).data = realloc((_da).data, sizeof(*(_da).data) * (_da).cap);\
 } while(0)
 
-#define darrayAllocSize(da) ((da).cap * sizeof(*(da).data))
+void darray_grow(void *darrayGeneric, usz target, usz typeSize);
 
-#define darrayAppend(da, el) do{\
-  if((da).size == (da).cap) darrayGrow(da);\
-  (da).data[(da).size] = el;\
-  ++(da).size;\
+#define darrayShrink(_da) do{\
+  while(((_da).cap >> 1ull) > (_da).size) (_da).cap >>= 1ull;\
+  (_da).data = safeRealloc((_da).data, sizeof(*(_da).data) * (_da).cap);\
 } while(0)
 
-#define darrayRemove(da, index) do{\
-  if(index < (da).size - 1)\
-    memcpy((da).data + (index), (da).data + (index) + 1, ((da).size - (index) - 1) * sizeof(*(da).data));\
-  --(da).size;\
+void darray_shrink(void *darrayGenereric, usz typeSize);
+
+#define darrayAppend(_da, _el) do{\
+  darrayGrow(_da, (_da).size + 1)\
+  (_da).data[(_da).size] = (_el);\
+  ++(_da).size;\
 } while(0)
+
+void darray_append(void *darrayGeneric, void *element, usz typeSize);
+
+void darray_appendPtr(void *darrayGeneric, void *element);
+
+#define darrayAppendMany(_da, _li, _n) do{\
+  (_da).size += (_n);\
+  darrayGrow(_da, (_da).size);\
+  memcpy((_da).data + (_da).size - _n, _li, _n * sizeof(*(_da).data));\
+} while(0)
+
+void darray_appendMany(void *darrayGeneric, void *elementList, usz elementCount, usz typeSize);
+
+#define darrayRemove(_da, index) do{\
+  if(index < (_da).size - 1)\
+    memmove(\
+      (_da).data + (index),\
+      (_da).data + (index) + 1,\
+      ((_da).size - (index) - 1) * sizeof(*(_da).data)\
+    );\
+  --(_da).size;\
+} while(0)
+
+void darray_remove(void *darrayGeneric, usz index, usz typeSize);
+
+#define darrayRemoveMany(_da, _index, _amount) do{\
+  if(_index < (_da).size - _amount)\
+    memmove(\
+      (_da).data + (_index),\
+      (_da).data + (_index) + (_amount),\
+      ((_da).size - (_index) - (_amount)) * sizeof(*(_da).data)\
+    );\
+  (_da).size -= (_amount);\
+} while(0)
+
+void darray_removeMany(void *darrayGeneric, usz index, usz amount, usz typeSize);
+
+#define darrayDestroy(_da) do{\
+  free((_da).data);\
+  memset(&(_da), 0, sizeof(_da));\
+} while(0)
+
+void darray_destroy(void *darrayGeneric);
+
+#define darrayRecFree(_da) do{\
+  for(usz i = 0; i < (_da).size; ++i)\
+    free((_da).data[i]);\
+} while(0)
+
+void darray_recFree(void *darrayGeneric);
+
+#define darrayInterate(_da, _type)\
+  for(usz _i = 0; _i < (_da).size; ++_i)\
+  for(_type _el = (_da).data[_i], *_once = NULL + 1; _once; _once = NULL)
 
 #endif
