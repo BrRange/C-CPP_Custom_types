ac77f55b45498661521b0744176a01642a9f5db7
BurstRange <113287175+BrRange@users.noreply.github.com>
2026-01-22 12:02:36 -0300

    Set function never before seen
    Abusing assembly fallthrough and the lea instruction

--- stringType.s
+++ stringType.s
@@ -1,52 +1,75 @@
 .intel_syntax noprefix
 
+.section rdata
+
 .text
 
 .global string_new
 string_new:
   push rbx
   push rbp
-  lea rsp, -32[rsp]
 
   mov rbx, rcx
   mov rbp, [rdx]
 
   mov ecx, 8[rdx]
   mov 8[rbx], ecx
-  lea ecx, 8[ecx]
+  lea ecx, 7[ecx]
   and ecx, -8
   mov 12[rbx], ecx
+  jz .string_new_return
 
+  lea rsp, -32[rsp]
   call malloc
+  lea rsp, 32[rsp]
+  
   mov [rbx], rax
-
   mov rcx, rax
   mov rdx, rbp
   mov r8d, 8[rbx]
 
-  lea rsp, 32[rsp]
   pop rbp
   pop rbx
   jmp memcpy
 
+.string_new_return:
+  mov [rbx], rcx
+  pop rbp
+  pop rbx
+  ret
+
+.global string_view
+string_view:
+  mov rax, [rdx]
+  mov [rcx], rax
+  mov eax, 8[rdx]
+  mov 8[rcx], eax
+  ret
+
+.global string_set
+string_set:
+  xor eax, eax
+  mov 8[rcx], eax
+
 .global string_append
 string_append:
-  push rbx
-  push rbp
-
-  mov rbx, rcx
-  mov rbp, [rdx]
+  mov rax, rdx
   mov edx, 8[rdx]
   test edx, edx
-  jz .string_append_ret
+  jz .string_append_return
 
-  push rdx
+  push rbx
+  push rbp
+  mov rbx, rcx
+  mov rbp, rax
+
+  push 8[rbx]
   add edx, 8[rbx]
   mov 8[rbx], edx
   lea edx, 7[edx]
   and edx, -8
-  cmp edx, 12[rbx]
-  jz .string_append_enoughCap
+  cmp 12[rbx], edx
+  jnc .string_append_enoughCap
 
   lea rsp, -32[rsp]
   mov rcx, [rbx]
@@ -57,15 +80,13 @@ string_append:
 
 .string_append_enoughCap:
   pop rcx
-  add rcx, rax
-  lea rcx, -1[rcx]
-  mov rdx, rbp
-  mov r8d, 8[rbx]
+  mov ecx, ecx
+  add rcx, [rbx]
+  mov rdx, [rbp]
+  mov r8d, 8[rbp]
   pop rbp
   pop rbx
   jmp memcpy
   
-.string_append_ret:
-  pop rbp
-  pop rbx
+.string_append_return:
   ret
